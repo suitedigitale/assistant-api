6ae944b4ff86f39b67d0a22e089433afe0422771
suitedigitale <hello@scalaabile.it>
2025-08-28 10:04:23 +0200

    Update sd-bundle.js

--- public/sd-bundle.js
+++ public/sd-bundle.js
@@ -4,12 +4,13 @@
 
   function load(u, next){
     var s = document.createElement('script');
-    s.src = u + (u.indexOf('?')>-1 ? '&' : '?') + 'v=6'; // bump per ricaricare CSS/JS
+    s.src = u + (u.indexOf('?')>-1 ? '&' : '?') + 'v=1'; // cache-bust
     s.defer = true;
     s.onload = function(){ next && next(); };
     document.head.appendChild(s);
   }
 
+  // 1) chat -> 2) triggers
   load(BASE + '/sd-chat.js', function(){ load(BASE + '/sd-triggers.js'); });
 
   try { document.dispatchEvent(new Event('SuiteAssistantLoaderReady')); } catch(e){}
